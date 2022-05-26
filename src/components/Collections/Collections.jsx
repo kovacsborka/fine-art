c15401b952a407626596ade18de0d762a8db68d6
kovacsborka <kovacsborbalaterez@gmail.com>
2022-05-26 12:45:38 +0200

    importing pictures

--- src/components/Collections/Collections.jsx
+++ src/components/Collections/Collections.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import Egypt from './egypt.jpg'
+import Classical from './classical.jpg'
+import OldMaster from './old_master.jpg'
+import Sculptures from './sculptures.jpg'
+
 
 function Collections() {
   return (
@@ -7,18 +12,22 @@ function Collections() {
       <div className='collection'>
         <h2>Egyptian Antiquities</h2>
         <p>The earliest artefacts of the Egyptian art collection found their way to Hungary thanks to nineteenth-century globe-trotters, art collectors, and art dealers. The idea to unite the ancient Egyptian artefacts preserved in several museums into one single collection emerged at the beginning of the twentieth century and was implemented in 1934, when the Egyptian collection of the Museum of Fine Arts was founded. Apart from the Museum of Fine Arts, Egyptian artefacts can only be found in Hungary in the Déri Museum in Debrecen, in the Reformed Collections of Pápa, and in the Savaria Museum/Iseum Savariense in Szombathely.</p>
+        <img src={Egypt} alt="" />
       </div>
       <div className='collection'>
         <h2>Classical Antiquities</h2>
         <p>The Collection of Classical Antiquities occupies a special position in Hungarian culture: it is the only comprehensive collection of ancient art in Hungary, which provides a unique opportunity for a direct encounter with the artistic heritage of classical antiquity. This fact defines the work conducted here: the Collection is both a department of the Museum of Fine Arts, and a research centre that focuses on classical archaeology. The two areas are inseparably related.</p>
+        <img src={Classical} alt="" />
       </div>
       <div className='collection'>
         <h2>Old Master Paintings</h2>
         <p>The almost 3,000 paintings of the collection is the richest in Italian works, including masterpieces by Raphael, Giorgione, Titian, Jacopo Tintoretto, and Giovanni Battista Tiepolo. Netherlandish painting is represented by great masters such as Petrus Christus and Gerard David, while the seventeenth-century “golden age” is introduced through the art of Anthony van Dyck, Jacob van Ruisdael, and Jacob Jordaens. The department preserves one of Europe’s most prominent Spanish collections, including paintings by Diego Velázquez, El Greco, Francisco Goya, and Bartolomé Esteban Murillo. The German and Central European parts of the collection are also remarkable both in regard to the number and quality of the works, including pictures by Albrecht Dürer, Hans Holbein the Elder, Lucas Cranach, Franz Anton Maulbertsch, and Jan Kupeczky. The baroque section showcasing the art of the French (Claude Lorrain, Nicolas Poussin) and British (Joshua Reynolds, John Constable) schools is more modest but full of interesting works.</p>
+        <img src={OldMaster} alt="" />
       </div>
       <div className='collection'>
         <h2>Sculptures</h2>
         <p>The collection comprising more than 600 works boasts treasures of Renaissance Italy including the exceptional Horse and Rider, attributed to Leonardo da Vinci, and the Man of Sorrows by Andrea del Verrocchio, but its small bronzes are also deservedly famous. Medieval German, Netherlandish, and French sculpture are represented by many masterpieces, such as the “Beautiful Madonnas” of the International Gothic style and the wooden sculptures from Tilman Riemenschneider’s workshop, while Austrian baroque art is showcased through works by Georg Rafael Donner and Franz Xaver Messerschmidt, among others.</p>
+        <img src={Sculptures} alt="" />
       </div>
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Logo from '../../logo-szepmu-eng.svg'
+import Logo from '../../logo-szepmu-eng1.svg'
 import './Navbar.css'
 
 function Navbar() {
   return (
     <div className='navbar'>
-        <ul>
+        <ul className='navbarlist'>
             <li> <img src={Logo} alt="" className='logo'/></li>
             <li> <Link to="/collections">  Collections</Link></li>
             <li> <Link to="/visit">  Visit</Link></li>
             <li> <Link to="/exhibitions">  Exhibitions</Link></li>
             <li> <Link to="/artworks">  Artworks</Link></li>
-            <li> <Link to="/mud">  Collections</Link></li>
+            <li> <Link to="/museumshop">  Museum shop </Link></li>
+            <li> Search </li>
             
         </ul>
     </div>
